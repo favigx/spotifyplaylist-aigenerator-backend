540f15cac08822219ff09b20cce64ae833771947
Viktor Falkesjö <viktorfalkesjo@gmail.com>
2024-10-16 10:34:01 +0200

    first commit, new project with dependencys

---
+++ src/main/java/com/spotifyplaylist_aigenerator_backend/spotifyplaylist_aigenerator_backend/SpotifyplaylistAigeneratorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spotifyplaylist_aigenerator_backend.spotifyplaylist_aigenerator_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyplaylistAigeneratorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyplaylistAigeneratorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spotifyplaylist_aigenerator_backend/spotifyplaylist_aigenerator_backend/SpotifyplaylistAigeneratorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotifyplaylist_aigenerator_backend.spotifyplaylist_aigenerator_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyplaylistAigeneratorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
