e6029559b28da88d1861110690b178d25144affb
Viktor Falkesjö <viktorfalkesjo@gmail.com>
2025-04-05 18:10:37 +0200

    crossorigin

--- src/main/java/com/spotifyplaylist_aigenerator_backend/spotifyplaylist_aigenerator_backend/integrations/payment/stripe/StripeController.java
+++ src/main/java/com/spotifyplaylist_aigenerator_backend/spotifyplaylist_aigenerator_backend/integrations/payment/stripe/StripeController.java
@@ -1,6 +1,7 @@
 package com.spotifyplaylist_aigenerator_backend.spotifyplaylist_aigenerator_backend.integrations.payment.stripe;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+@CrossOrigin("*")
 @RequestMapping("/api/stripe")
 @RestController
 public class StripeController {
