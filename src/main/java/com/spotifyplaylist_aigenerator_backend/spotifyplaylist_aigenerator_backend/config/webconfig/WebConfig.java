9e13128fcac8143ad063129d12851a21b8bf50f2
Viktor Falkesjö <viktorfalkesjo@gmail.com>
2025-04-15 11:15:24 +0200

    .

--- src/main/java/com/spotifyplaylist_aigenerator_backend/spotifyplaylist_aigenerator_backend/config/webconfig/WebConfig.java
+++ src/main/java/com/spotifyplaylist_aigenerator_backend/spotifyplaylist_aigenerator_backend/config/webconfig/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://lobster-app-ebdey.ondigitalocean.app", "http://localhost:5173",
-                        "http://192.168.50.248:8081", "http://localhost:8081",
+                        "http://192.168.50.248:8081", "http://localhost:8081", "exp://192.168.50.248:8081",
                         "https://sea-turtle-app-le797.ondigitalocean.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
